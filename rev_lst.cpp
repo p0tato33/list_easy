489be990bddb651e0ff2e9820185a89f9ded48d0
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-12-16 00:03:06 +0300

    Create rev_lst.cpp

---
+++ rev_lst.cpp
@@ -0,0 +1,21 @@
+void itc_rev_list(vector <int>& mass)
+{
+    int c;
+    for (int i = 0; i < mass.size() / 2;i++)
+    {
+        c = mass[mass.size() - 1 - i];
+        mass[mass.size() - 1 - i] = mass[i];
+        mass[i] = c;
+    }
+}
+
+void itc_rev_par_list(vector <int>& mass)
+{
+    int c;
+    for (int i = 0; i < (mass.size() - mass.size() % 2);i += 2)
+    {
+        c = mass[i];
+        mass[i] = mass[i + 1];
+        mass[i + 1] = c;
+    }
+}
