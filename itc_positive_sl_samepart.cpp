296b19e30380d86eaaac668652b5f851d65cbb87
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-12-16 20:33:21 +0300

    Update itc_positive_sl_samepart.cpp

--- itc_positive_sl_samepart.cpp
+++ itc_positive_sl_samepart.cpp
@@ -1,39 +1,38 @@
 #include "easy_list.h"
 
-int itc_positive_list(const vector <int>& mass)
-{
+int itc_positive_list(const vector <int> &mass){
+    int sz = mass.size();
     int c = 0;
-    for(int i = 0; i < mass.size();i++)
-    {
-        if(mass[i] > 0)
-        {
-            c++;        //c++! 
+    if(sz != 0){
+        for(int i = 0; i < sz; i++){
+            if(mass[i] >= 0){
+                c++;
+            }
         }
     }
     return c;
 }
-
-int itc_sl_list(const vector <int>& mass)
-{
-    int c = 0;
-    for(int i = 1; i < mass.size();i++)
-    {
-        if(mass[i] > mass[i - 1])
-        {
-            c++;
+int itc_sl_list(const vector <int> &mass) {
+    if (mass.size() != 0){
+        int c = 0;
+        for (int i = 1; i < mass.size(); i++){
+            if (mass[i] > mass[i - 1]){
+                c++;
+            }
         }
+        return c;
     }
-    return c;
 }
 
-bool itc_same_parts_list(const vector <int>& mass)
-{
-    for(int i = 1; i < mass.size();i++)
-    {
-        if(mass[i] == mass[i - 1])
-        {
-            return true;
+bool itc_same_parts_list(const vector <int> &mass){
+    if (mass.size() != 0){
+        for (int i = 1; i < mass.size(); i++){
+            if (mass[i] >= 0 && mass[i - 1] >= 0)
+                return true;
+            if (mass[i] < 0 &&  mass[i - 1] < 0)
+                return true;
         }
+        return false;
     }
-    return false;
+
 }
