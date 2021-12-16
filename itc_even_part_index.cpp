f792f5448548c381db7e9c5711e9a6cabfdc6667
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-12-16 20:28:26 +0300

    Update itc_even_part_index.cpp

--- itc_even_part_index.cpp
+++ itc_even_part_index.cpp
@@ -1,22 +1,14 @@
 #include "easy_list.h"
-void itc_even_index_list(const vector <int>& mass, vector <int>& mass2)
-{
-    for(int i = 0; i < mass.size();i++)
-    {
-        if(mass[i] % 2 == 0)
-        {
+
+void itc_even_index_list(const vector <int> &mass, vector <int> &mass2) {
+    if (mass.size() != 0)
+        for (int i = 0; i < mass.size(); i += 2)
             mass2.push_back(mass[i]);
-        }
-    }
 }
 
-void itc_even_parts_list(const vector <int>& mass, vector <int>& mass2)
-{
-    for(int i = 0; i < mass.size();i++)
-    {
-        if(i % 2 == 0)
-        {
-            mass2.push_back(mass[i]);
-        }
-    }
+void itc_even_parts_list(const vector<int> &mass, vector<int> &mass2) {
+    if (mass.size() != 0)
+        for(int i = 0; i < mass.size(); i++)
+            if(mass[i] % 2 == 0)
+                mass2.push_back(mass[i]);
 }
